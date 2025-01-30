76480e91989f3003dae93ffe021c66c72be1f4e2
sakura1bgx <isakura710@gmail.com>
2025-01-30 20:09:20 +0900

    Update flipstreamviewer.py
    fix to clean warning

--- flipstreamviewer.py
+++ flipstreamviewer.py
@@ -1856,7 +1856,7 @@ class FlipStreamSegMask:
                 input_ids=inputs["input_ids"].cuda(),
                 pixel_values=inputs["pixel_values"].cuda(),
                 max_new_tokens=1024,
-                early_stopping=True,
+                early_stopping=False,
                 do_sample=False,
                 num_beams=1,
             )
